38f3a6ec2efd1dd1109b41869f7c8edfe093d38e
sachseka <sachseka@hu-berlin.de>
2021-09-08 15:27:01 +0200

    some more Tests for automateDataPreparation

--- tests/testthat/test-automateDataPreparation.R
+++ tests/testthat/test-automateDataPreparation.R
@@ -46,11 +46,27 @@ test_that("load sav", {
 
 test_that("merge", {
   path <- gsub("forcedChoice.sav", "", system.file("extdata", "forcedChoice.sav", package = "eatPrep"))
-  expect_message(automateDataPreparation(inputList = inputList[1:3], oldIDs = c("ID", "ID"),newID="idstud",
+  expect_message(ati <- automateDataPreparation(inputList = inputList[1:3], oldIDs = c("ID", "ID"),newID="idstud",
                                          datList = NULL,	path = path,
                                          readSpss = TRUE, checkData = FALSE,	mergeData = TRUE,
                                          recodeData = FALSE, recodeMnr = FALSE,
                                          aggregateData = FALSE, scoreData = FALSE,
                                          writeSpss = FALSE, verbose = TRUE),
                  "Start merging.")
+  expect_equal(ati[1,], data.frame(idstud="1", mcvar="3", stringvar="", mc="2", string="new valid", stringsAsFactors=FALSE))
+  expect_equal(dim(ati), c(12, 5))
+  expect_message(ati2 <- automateDataPreparation(inputList = inputList[1:3], oldIDs = c("ID", "ID"),newID="idstud",
+                                                datList = NULL,	path = path,
+                                                readSpss = TRUE, checkData = FALSE,	mergeData = FALSE,
+                                                recodeData = FALSE, recodeMnr = FALSE,
+                                                aggregateData = FALSE, scoreData = FALSE,
+                                                writeSpss = FALSE, verbose = TRUE),
+                 "Merge has been skipped, but more than one dataset has been loaded. A list of datasets will be returned.")
+  expect_warning(ati3 <- automateDataPreparation(inputList = inputList,
+                                                 datList = inputDat,
+                                                 readSpss = FALSE, checkData = FALSE,	mergeData = FALSE,
+                                                 recodeData = TRUE, recodeMnr = FALSE,
+                                                 aggregateData = FALSE, scoreData = FALSE,
+                                                 writeSpss = FALSE, verbose = TRUE),
+                 "Merge has been skipped. Only the first dataset in datList will be considered for the following steps.")
 })
