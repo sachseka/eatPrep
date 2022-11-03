84e3a5b3da7241f6e75cf988d79d7e0f730dee48
Karoline Sachse <sachseka@user.hu-berlin.de>
2022-11-03 16:52:30 +0100

    compact messages in recodeData

--- R/recodeData.R
+++ R/recodeData.R
@@ -8,9 +8,33 @@ recodeData <- function (dat, values, subunits = NULL, verbose = FALSE) {
     recodeinfo <- values
   }
 
-    datR <- data.frame(mapply(.recodeData.recode, dat,
+
+  info <- data.frame(mapply(.recodeData.info, dat,
   colnames(dat), MoreArgs = list(recodeinfo = recodeinfo, mode = "recode", verbose = verbose), USE.NAMES = TRUE),
   stringsAsFactors = FALSE)
+
+ info2 <- lapply(info, data.frame)
+  info2 <- eatTools::rbind_common(info2)
+
+  if(sum(!is.na(info2$incomplRecInfo))>0) {
+    message(paste0("Incomplete recode information for variable(s): \n",
+          paste0(info2[!is.na(info2$incomplRecInfo),"incomplRecInfo"], collapse = ",\n"),".\n-- This/These value(s) occuring in data but WITHOUT recode info will not be recoded."))
+  }
+
+  if(verbose) {
+    if(sum(!is.na(info2$noRecInfo))>0) {
+      message(paste0("\nFound no recode information for variable(s): \n", paste(info2[!is.na(info2$noRecInfo),"noRecInfo"], collapse = ", "), ". \nThis/These variable(s) will not be recoded."))
+    }
+    if(sum(!is.na(info2$recoded))>0) {
+      message(paste0("\nVariables... ", paste(info2[!is.na(info2$recoded),"recoded"], collapse = ", "), "\n...have been recoded."))
+    }
+  }
+
+
+  datR <- data.frame(mapply(.recodeData.recode, dat,
+  colnames(dat), MoreArgs = list(recodeinfo = recodeinfo, mode = "recode", verbose = verbose), USE.NAMES = TRUE),
+  stringsAsFactors = FALSE)
+
   if(!is.null(subunits))
     colnames(datR) <- eatTools::recodeLookup(colnames(datR), subunits[ , c("subunit", "subunitRecoded")])
 
@@ -34,21 +58,52 @@ recodeData <- function (dat, values, subunits = NULL, verbose = FALSE) {
 
   if (nrow(recinfoVar) == 0) {
     variableRecoded <- variable
+  } else {
+    lookupVar <- recinfoVar[ , c("value", "valueRecode") ]
+    variableRecoded <- eatTools::recodeLookup(variable, lookupVar)
+  }
+  return(variableRecoded)
+}
+
+#-----------------------------------------------------------------------------------------
+
+.recodeData.info <- function (variable, variableName, recodeinfo, dontcheck = "mbd", mode = c("recode", "score"), verbose = TRUE) {
+  variableRecoded <- NULL
+
+  if (!is.character(variable)) {
+    variable <- as.character(variable)
+  }
+
+  if(any(colnames(recodeinfo) == "subunit")) {
+    recinfoVar <- recodeinfo[which(recodeinfo$subunit == variableName), ]
+  } else {
+    recinfoVar <- recodeinfo[which(recodeinfo$unit == variableName), ]
+  }
+  retList <- data.frame(noRecInfo = NA, incomplRecInfo = NA, recoded = NA)
+  if (nrow(recinfoVar) == 0) {
+    variableRecoded <- variable
     if (mode == "recode"){
-      message(paste("Found no recode information for variable ", variableName, ". This variable will not be recoded.", sep =""))
+      retList[1,"noRecInfo"] <- variableName
+      #message(paste("Found no recode information for variable ", variableName, ". This variable will not be recoded.", sep =""))
     }
   } else {
     variable.unique <- na.omit(unique(variable[which(!variable %in% dontcheck)]))
     recodeinfoCheck <- (variable.unique %in% recinfoVar$value)
     if (!all(recodeinfoCheck == TRUE)) {
-      warning(paste("Incomplete recode information for variable ",
-      variableName, ". Value(s) ",
-      paste(sort(variable.unique[!recodeinfoCheck]), collapse = ", "), " will not be recoded.", sep = ""))
+      if(length(variable.unique[!recodeinfoCheck]) > 10) {
+        b1 <- length(variable.unique[!recodeinfoCheck]) - 10
+        a1 <- paste0(variableName, " (Value/s: ", paste(sort(variable.unique[!recodeinfoCheck])[1:10], collapse = ", "), " ...and ", b1, " more.)")
+      } else {
+        a1 <- paste0(variableName, " (Value/s: ", paste(sort(variable.unique[!recodeinfoCheck]), collapse = ", "), ")")
+      }
+      retList[1,"incomplRecInfo"] <- a1
+      #warning(paste("Incomplete recode information for variable ",
+             #       variableName, ". Value(s) ",
+            #        paste(sort(variable.unique[!recodeinfoCheck]), collapse = ", "), " will not be recoded.", sep = ""))
     }
-
-    lookupVar <- recinfoVar[ , c("value", "valueRecode") ]
-    variableRecoded <- eatTools::recodeLookup(variable, lookupVar)
-	if (verbose) message(paste(variableName, " has been recoded.", sep =""))
+    retList[1,"recoded"] <- variableName
+    #if (verbose) message(paste(variableName, " has been recoded.", sep =""))
   }
-  return(variableRecoded)
+
+  return(retList)
 }
--- tests/testthat/test-recodeData.R
+++ tests/testthat/test-recodeData.R
@@ -11,13 +11,15 @@ test_that("recoded data preserves all rows and columns", {
 test_that("throws warning if a data point value has no recode value", {
   tempdat <- inputDat[[1]][ , -c(1,2)]
   tempdat$I06[10] <- 5
-  expect_warning(recodeData(tempdat, values = inputList$values, subunits = inputList$subunits),
-                 "Incomplete recode information for variable I06\\. Value\\(s\\) 5 will not be recoded\\." )
+  expect_message(recodeData(tempdat, values = inputList$values, subunits = inputList$subunits),
+                 "Incomplete recode information for variable(s): \nI06 (Value/s: 5).\n-- This/These value(s) occuring in data but WITHOUT recode info will not be recoded.", fixed=TRUE )
 })
 
 test_that("throws message if a variable cannot be recoded", {
   tempdat <- inputDat[[1]][ , -1]
-  expect_message(recodeData(tempdat, inputList$values, inputList$subunits), "Found no recode information for variable hisei" )
+  test <- capture_messages(recodeData(tempdat, inputList$values, inputList$subunits, verbose=TRUE))
+  expect_equal(test[1],"\nFound no recode information for variable(s): \nhisei. \nThis/These variable(s) will not be recoded.\n")
+  expect_equal(test[2],"\nVariables... I01, I02, I03, I04, I05, I06, I07, I08, I09, I10, I11, I12a, I12b, I12c, I13, I14, I15, I16, I17, I18, I19, I20, I21\n...have been recoded.\n")
 })
 
 test_that("recode works without subunits sheet, colnames are not recoded", {
