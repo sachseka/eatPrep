a549037f315757c2066f54539ea0b898b44c0eb1
sachseka <sachseka@hu-berlin.de>
2024-03-19 12:54:18 +0100

    full functionality for scored eatPrepSets

--- R/prep2gads.R
+++ R/prep2gads.R
@@ -1,20 +1,23 @@
-prep2gads <- function (dat, inputList, verbose = TRUE) {
+prep2gads <- function (dat, inputList, scored = TRUE, misTypes = list(mvi = -95, mnr = -96, mci = -97,
+                                                                      mbd = -94, mir = -98, mbi = -99), verbose = TRUE) {
 
   checkmate::assert_data_frame(dat)
   checkmate::assert_list(inputList, min.len = 3)
   checkmate::assert_data_frame(inputList$units)
   checkmate::assert_data_frame(inputList$subunits)
   checkmate::assert_data_frame(inputList$values)
+  checkmate::assert_list(misTypes, min.len = 1)
+  checkmate::assert_logical(scored, len=1)
+  checkmate::assert_logical(verbose, len=1)
 
   # if(any(sapply(dat, is.factor))) stop("At least one of the variables in df is a factor. This is unusual in eatPrep and therefore handling is not implemented.")
   units <- inputList$units
   subunits <- inputList$subunits
   values <- inputList$values
 
-  if(all(names(dat) %in%  units$unit)) {
-    labels1 <- data.frame(varName= names(dat), varLabel= units$unitLabel[match(names(dat), units$unit)], format=NA, display_width=NA, labeled=NA)
-  } else {
-    if(verbose) {
+  labels1 <- data.frame(varName= names(dat), varLabel= units$unitLabel[match(names(dat), units$unit)], format=NA, display_width=NA, labeled=NA)
+
+  if(!all(names(dat) %in%  units$unit) & verbose) {
       ntoOmitDat <- length(setdiff(names(dat), units$unit))
       toOmitDat <- setdiff(names(dat), units$unit)
       cli_h3("{.strong Check:} Variables without info")
@@ -25,73 +28,56 @@ prep2gads <- function (dat, inputList, verbose = TRUE) {
                      {.envvar {toOmitDat}}",
                      wrap = TRUE)
     }
-  }
 
   # add unit names to values-df
   values$unit <- sapply(seq(along=values$subunit), function(i) unique(subunits$unit[subunits$subunit == values$subunit[i]]))
 
-  subs <- unique(subunits$unit[which(subunits$unit != subunits$subunit)])
+  # units with subunits
+  # subs <- unique(subunits$unit[which(subunits$unit != subunits$subunit)])
 
-  values2 <- values[!duplicated(paste0(values$subunit,values$valueRecode)),]
-  values2 <- data.frame(varName=values2$subunit, value=values2$valueRecode, valLabel=values2$valueLabel, missings=ifelse(grepl("^m",values2$valueType),"miss","valid"))
-  values2$valLabel[values2$value=="0"] <- "other"
-  values2$valLabel[values2$value=="1" & values2$varName %in% subs] <- "right answer"
+  # reduce values info
+  # values2$valLabel[values2$value=="0"] <- "other"
+  # values2$valLabel[values2$value=="1" & values2$varName %in% subs] <- "right answer"
+  if(scored) {
+    for(uu in unique(values$unit)) {
+      if(nrow(values[values$unit==uu & values$valueRecode == 0,])>1) {
+        if(!all(values$subunit[values$unit==uu] == uu)) {
+          values$valueLabel[values$unit==uu & values$valueRecode == 0] <- "wrong answer (aggregated)"
+        } else {
+          values$valueLabel[values$unit==uu & values$valueRecode == 0] <- paste(values$valueLabel[values$unit==uu & values$valueRecode == 0], collapse= "', or '")
+        }
+      }
+      if(nrow(values[values$unit==uu & values$valueRecode == 1,])>1) {
+        if(!all(values$subunit[values$unit==uu] == uu)) {
+          values$valueLabel[values$unit==uu & values$valueRecode == 1] <- "right answer (aggregated)"
+        } else {
+          values$valueLabel[values$unit==uu & values$valueRecode == 1] <- paste(values$valueLabel[values$unit==uu & values$valueRecode == 1], collapse= "', or '")
+        }
+      }
+    }
+    values2 <- values[!duplicated(paste0(values$unit,values$valueRecode)),]
+  } else {
+    values2 <- values[!duplicated(paste0(values$unit,values$value)),]
+  }
 
+  # restructure
+  if(scored) {
+     values3 <- data.frame(varName=values2$unit, value=values2$valueRecode, valLabel=values2$valueLabel, missings=ifelse(grepl("^m",values2$valueType, ignore.case=TRUE),"miss","valid"))
+  } else {
+    values3 <- data.frame(varName=values2$unit, value=values2$value, valLabel=values2$valueLabel, missings=ifelse(grepl("^m",values2$valueType, ignore.case=TRUE),"miss","valid"))
+    ### BAUSTELLE MISSING RECODING WON'T WORK
+  }
 
-  labels2 <- dplyr::full_join(labels1, values2, by="varName")
+  labels2 <- dplyr::full_join(values3, labels1, by="varName")
+  labels2 <- labels2[,c("varName", "varLabel", "format", "display_width", "labeled", "value", "valLabel", "missings")]
+  labels2$labeled <- ifelse(!is.na(labels2$varLabel) | !is.na(labels2$valLabel), "yes", "no")
+  suppressWarnings(labels2 <- collapseMissings(labels2, missing.rule = misTypes, standard=FALSE))
 
-  prepGADS <- new_GADSdat(dat = dat, labels = labels2)
+  dat2 <- collapseMissings(dat, missing.rule = misTypes, standard=FALSE)
+  suppressWarnings(dat2 <- eatTools::asNumericIfPossible(dat2, force.string=FALSE))
+
+  prepGADS <-list(dat = dat2, labels = labels2)
 
   return(prepGADS)
 }
 
-
-# re-use some internal eatGADS-Checks
-new_GADSdat <- function(dat, labels) {
-  stopifnot(is.data.frame(dat) && is.data.frame(labels))
-  structure(list(dat = dat, labels = labels), class = c("GADSdat", "list"))
-}
-# GADSdat validator (allow data_table column for trend compatibility)
-check_GADSdat <- function(GADSdat) {
-  if(!"GADSdat" %in% class(GADSdat)) stop("All input objects have to be of class GADSdat", call. = FALSE)
-  if(!is.list(GADSdat) && length(GADSdat) == 2) stop("GADSdat has to be a list with length two", call. = FALSE)
-  if(!identical(names(GADSdat), c("dat", "labels"))) stop("List elements of a GADSdat object have to be 'dat' and 'labels'", call. = FALSE)
-  if(!is.data.frame(GADSdat$dat)) stop("dat element has to be a data frame", call. = FALSE)
-  if(tibble::is_tibble(GADSdat$dat)) stop("dat element has to be a data frame and can not be a tibble.", call. = FALSE)
-  if(!is.data.frame(GADSdat$labels)) stop("labels element has to be a data frame", call. = FALSE)
-  if(!(identical(names(GADSdat$labels), c("varName", "varLabel", "format", "display_width", "labeled", "value", "valLabel", "missings")) ||
-       identical(names(GADSdat$labels), c("varName", "varLabel", "format", "display_width", "labeled", "value", "valLabel", "missings", "data_table")))) {
-    stop("Illegal column names in labels data frame.")
-  }
-
-  # internals
-  only_in_labels <- setdiff(unique(GADSdat$labels$varName), names(GADSdat$dat))
-  only_in_dat <- setdiff(names(GADSdat$dat), unique(GADSdat$labels$varName))
-  if(length(only_in_labels) > 0) stop("The following variables have meta data but are not in the actual data: ", only_in_labels, call. = FALSE)
-  if(length(only_in_dat) > 0) stop("The following variables are in the data but do not have meta data: ", only_in_dat, call. = FALSE)
-
-  if(!is.numeric(GADSdat$labels$value)) stop("Column 'value' in the meta data is not numeric.")
-
-  unlabeled_labels <- GADSdat$labels[GADSdat$labels$labeled == "no", ]
-  if(nrow(unlabeled_labels) > 0) {
-    by(unlabeled_labels, unlabeled_labels$varName, function(labels) {
-      if(any(!is.na(labels$value))) stop("The following variable has value labels but is not marked as labeled: ", unique(labels$varName))
-      if(any(!is.na(labels$valLabel))) stop("The following variable has value labels but is not marked as labeled: ", unique(labels$varName))
-    })
-  }
-
-}
-
-check_GADSdat_varLevel_meta <- function(GADSdat) {
-  # maybe later remove these tests due to performance?
-  by(GADSdat$labels, GADSdat$labels$varName, function(labels) {
-    if(any(duplicated(labels$value[!is.na(labels$value)]))) {
-      stop("The following variable has duplicate values rows in its meta data: ", labels$varName[1])
-    }
-    unique_lengthes <- length(unique(labels$varLabel)) + length(unique(labels$format)) + length(unique(labels$labeled))
-    if(unique_lengthes != 3) {
-      stop("The following variable has inconsistent meta information on variable level: ", labels$varName[1])
-    }
-
-  })
-}
