999178da37626f1b4ed78eee7dfb09f991c6693e
Edna Gwendolyn Daphne Grewers <grewered@user.hu-berlin.de>
2024-04-04 14:20:16 +0200

    updated assertion

--- R/cat.pbc.R
+++ R/cat.pbc.R
@@ -1,7 +1,7 @@
 catPbc <- function(datRaw, datRec, idRaw, idRec, context.vars = NULL,
                    values, subunits, file.name = NULL, verbose = FALSE) {
   lapply(c(datRaw, datRec), checkmate::assert_data_frame)
-  lapply(c(idRaw, idRec), checkmate::assert_character)
+  lapply(c(idRaw, idRec), checkmate::assert_scalar)
   checkmate::assert_vector(context.vars, null.ok = TRUE)
   lapply(c(values, subunits), checkmate::assert_list, types = "data.frame")
   checkmate::assert_logical(verbose, len = 1)
