0c469d7c3a7e945f2d34ee385ddad842889d192d
grewered <grewered@hu-berlin.de>
2024-04-05 13:30:45 +0200

    added some assertions: automateDataPrep

--- R/automateDataPreparation.R
+++ R/automateDataPreparation.R
@@ -7,6 +7,19 @@ automateDataPreparation <- function(datList = NULL, inputList, path = NULL,
 						addLeadingZeros=FALSE, truncateSpaceChar = TRUE, newID = NULL, oldIDs = NULL,
             missing.rule = list(mvi = 0, mnr = 0, mci = NA, mbd = NA, mir = 0, mbi = 0), verbose=FALSE) {
 
+  checkmate::assert_list(datList, types = "data.frame", null.ok = TRUE)
+  checkmate::assert_list(inputList, types = "data.frame")
+  checkmate::assert_character(path, len = 1, null.ok = TRUE)
+  lapply(c(filedat, filesps), checkmate::assert_character, len = 1)
+  lapply(c(readSpss, checkData, mergeData, recodeData, recodeMnr,
+           aggregateData, scoreData, writeSpss, collapseMissings, suppressErr), checkmate::assert_logical, len = 1)
+  checkmate::assert_numeric(breaks, null.ok = TRUE)
+  checkmate::assert_numeric(nMbi, len = 1, lower = 1)
+  checkmate::assert_scalar(rotation.id, null.ok = TRUE)
+  checkmate::assert_character(recodeErr, len = 1)
+
+  #missing.rule
+
 		###folder erstellen
 		if(is.null(path)) {
 		  folder.e <- getwd()
