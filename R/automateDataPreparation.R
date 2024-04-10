3b1603f8423d8e83451c01bc1a0f01c5fdc86829
grewered <grewered@hu-berlin.de>
2024-04-10 16:44:18 +0200

    new check datList

--- R/automateDataPreparation.R
+++ R/automateDataPreparation.R
@@ -7,7 +7,11 @@ automateDataPreparation <- function(datList = NULL, inputList, path = NULL,
 						addLeadingZeros=FALSE, truncateSpaceChar = TRUE, newID = NULL, oldIDs = NULL,
             missing.rule = list(mvi = 0, mnr = 0, mci = NA, mbd = NA, mir = 0, mbi = 0), verbose=FALSE) {
 
-  checkmate::assert_list(datList, types = "data.frame", null.ok = TRUE)
+  if (checkmate::test_list(datList)){
+    checkmate::assert_list(datList, types = "data.frame", null.ok = TRUE)
+  } else {
+    checkmate::assert_data_frame(datList, null.ok = TRUE)
+  }
   checkmate::assert_list(inputList, types = "data.frame", null.ok = TRUE)
   checkmate::assert_character(path, len = 1, null.ok = TRUE)
   lapply(c(filedat, filesps), checkmate::assert_character, len = 1)
